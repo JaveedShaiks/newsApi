91ce181051ae2cfebb244c082457bde970337fa7
javeedShaiks <javeed.sunshine@gmail.com>
2020-06-25 11:45:01 +0100

    updated port

--- src/index.js
+++ src/index.js
@@ -12,17 +12,22 @@ app.get('/', (req, res) => {
   const html = `
    <html>
    <head>
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+   <link rel="stylesheet" href="styles.css" >
+   </head>
    <body>
    <div id='root'>${content}</div>
    <script src="bundle.js"></script>
    </body>
-   </head>
+
    </html>
     `;
 
   res.send(html);
 });
 
-app.listen(4048, () => {
+const port = process.env.PORT || 4048;
+
+app.listen(port, () => {
   console.log('server running on port 4048');
 });
