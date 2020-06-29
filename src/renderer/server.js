4b2c06a2d1bed41eed4b3857f3600d9c47e5eb9c
javeedShaiks <javeed.sunshine@gmail.com>
2020-06-29 03:29:51 +0100

    server file changes

--- src/renderer/server.js
+++ src/renderer/server.js
@@ -16,13 +16,16 @@ export default (req, store, context) => {
   );
 
   return `
-    <html>
+    <html lang="en">
       <head>
       <title>Hacker News </title>
       <meta name="viewport"
-      content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no">
+      content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+      <link rel="preload" href="styles.css" as="style">
+      <link rel="preload" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" as="style">
        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
        <link rel="stylesheet" href="styles.css" >
+       <link rel="preload" href="bundle.js" as="script">
       </head>
       <body>
         <div id="root">${content}</div>
